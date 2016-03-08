e46a1f2d515a34dc184e02bd1a779b5914c30aae
Stav Pan Geffen <stavgeffen@gmail.com>
2016-03-08 11:27:33 +0200

    FourOFour: Removed tracking for angularState routing

--- server/middleware/four-o-four.js
+++ server/middleware/four-o-four.js
@@ -7,9 +7,13 @@ module.exports = function(req, res){
 
 	// respond with html page
 	if (req.accepts('html')){
-		res.status(isFrontEndRoute(req.path) ? 200 : 404).sendFile('partials/index.html', { root: 'public', maxAge: 0 });
+		const angularState = isFrontEndRoute(req.path);
 
-		return req.track({ cat: '404', label: 'http', path: req.path });
+		res.status(angularState ? 200 : 404).sendFile('partials/index.html', { root: 'public', maxAge: 0 });
+
+		if (!angularState) return req.track({ cat: '404', label: 'http', path: req.path });
+
+		return;
 	}
 
 	// respond with json
