207e1ff064a0f75e02d59a6685d525704bae19d1
Stav Pan Geffen <stavgeffen@gmail.com>
2016-05-01 13:39:26 +0300

    Index: Set port to const

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-'use strict';
-
-var log = require('./server/services/log');
+const log = require('./server/services/log');
+const port = process.env.PORT || 3000;
 
 require('./env.js');
 
-let port = process.env.PORT || 3000;
 require('./server')
 	.init()
 	.listen(port, () => log.info(`Server is up! Listening on ${port}.`));
