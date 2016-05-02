3c933b7604a5dce007cf7ffa728fd302686487f0
Stav Pan Geffen <stavgeffen@gmail.com>
2016-05-02 16:05:06 +0300

    SuggestionsList: Fixed header numbering

--- client/src/states/draft/modals/suggestions-list.ctrl.js
+++ client/src/states/draft/modals/suggestions-list.ctrl.js
@@ -6,4 +6,16 @@ function controller(mem){
 
 	this.suggestions = mem('paraSuggestions', null, { scoped: true }) || {};
 
+	this.header = numberOf(ctrl.suggestions);
+
+	function numberOf(suggestions){
+
+		if (!suggestions.length) return 'אין הצעות';
+
+		if (suggestions.length === 1) return 'הצעה אחת';
+
+		return suggestions.length + ' הצעות';
+
+	}
+
 }
