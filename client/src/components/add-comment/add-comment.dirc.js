279e39e87d0ed1209776c7044e9f760c3adddc62
Stav Pan Geffen <stavgeffen@gmail.com>
2016-05-03 19:56:34 +0300

    Draft: Added addComment component

---
+++ client/src/components/add-comment/add-comment.dirc.js
@@ -0,0 +1,18 @@
+const template = require('./add-comment.jade');
+
+module.exports = [directive];
+
+function directive(){
+	return {
+
+		scope: {
+			content: '=',
+			send: '&'
+		},
+
+		restrict: 'E',
+
+		template
+
+	};
+}
--- client/src/states/draft/draft.ctrl.js
+++ client/src/states/draft/draft.ctrl.js
@@ -1,6 +1,6 @@
-module.exports = ['modal', controller];
+module.exports = ['$scope', 'modal', controller];
 
-function controller(modal){
+function controller($scope, modal){
 
 	const ctrl = this;
 
@@ -256,5 +256,23 @@ function controller(modal){
 		} });
 
 	};
+
+	this.addComment = () => {
+
+		const newComment = {
+			content: ctrl.newComment,
+			author: {
+				displayName: 'שם המחבר',
+				avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/nuraika/128.jpg'
+			},
+			date: '08.07.16',
+			hour: '17:14',
+			createdAt: new Date('07.08.16')
+		};
+
+		ctrl.comments.push(newComment);
+
+		ctrl.newCommentPosted = true;
+	}
 
 }
